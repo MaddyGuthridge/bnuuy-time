64726ad5f4e075688468603c91798b118f5b4e73
Maddy Guthridge <hello@maddyguthridge.com>
2025-04-19 23:42:44 +1000

    Require bun time to match exactly so it looks better

--- bnuuy_time/buns.py
+++ bnuuy_time/buns.py
@@ -44,35 +44,6 @@ def load_buns() -> list[BunDefinition]:
         return json.load(f)
 
 
-# buns = load_buns()
-
-
-def bun_left_ear_range(bun: BunDefinition) -> list[int]:
-    oclock = bun["left_ear"]
-    if oclock == 6:
-        return [6, 7]
-    elif oclock == 12:
-        return [11, 12]
-    elif oclock == 1:
-        return [12, 1]
-    else:
-        return list(range(oclock - 1, oclock + 2))
-
-
-def bun_right_ear_range(bun: BunDefinition) -> list[int]:
-    oclock = bun["right_ear"]
-    if oclock == 6:
-        return [5, 6]
-    elif oclock == 12:
-        return [12, 1]
-    elif oclock == 1:
-        return [12, 1, 2]
-    elif oclock == 11:
-        return [11, 12]
-    else:
-        return list(range(oclock - 1, oclock + 2))
-
-
 def bun_matches(time: datetime, bun: BunDefinition) -> bool:
     """
     Returns whether this bun matches the given time
@@ -84,14 +55,14 @@ def bun_matches(time: datetime, bun: BunDefinition) -> bool:
     if minute_oclock == 0:
         minute_oclock = 12
 
-    left_ear = bun_left_ear_range(bun)
-    right_ear = bun_right_ear_range(bun)
+    left_ear = bun["left_ear"]
+    right_ear = bun["right_ear"]
 
     # Left ear is hour hand, right ear is minute hand
-    if hour_oclock in left_ear and minute_oclock in right_ear:
+    if hour_oclock == left_ear and minute_oclock == right_ear:
         return True
     # Left ear is minute hand, right ear is hour hand
-    elif hour_oclock in right_ear and minute_oclock in left_ear:
+    elif hour_oclock == right_ear and minute_oclock == left_ear:
         return True
     else:
         return False
