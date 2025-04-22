037ac62b8d3a3323be567b67b0af89da2d837c47
Maddy Guthridge <hello@maddyguthridge.com>
2025-04-23 01:24:43 +1000

    Add test cases

---
+++ tests/angles_test.py
@@ -0,0 +1,16 @@
+import pytest
+
+from bnuuy_time.buns import angle_diff
+
+
+@pytest.mark.parametrize(
+    ["a", "b", "expected"],
+    [
+        [1, 2, 1],
+        [2, 1, 1],
+        [359, 0, 1],
+        [0, 359, 1],
+    ]
+)
+def test_angle_diff(a, b, expected):
+    assert angle_diff(a, b) == expected
